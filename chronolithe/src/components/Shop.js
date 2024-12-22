9914fd91a60265b9ca6f137be5e2d2bc070baf96
maxime-jcnl <maxime.jaconelli@efrei.net>
2024-12-23 00:45:13 +0100

    modif page shop

--- chronolithe/src/components/Shop.js
+++ chronolithe/src/components/Shop.js
@@ -1,48 +1,58 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Style.css";
-import Navbar from "./Navbar";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
+  const [activeImage, setActiveImage] = useState({}); // État pour gérer l'image active
 
   useEffect(() => {
     // Charger les produits depuis le fichier JSON
     fetch("/products.json")
       .then((response) => response.json())
-      .then((data) => {
-        console.log("Données chargées :", data); // Vérifie les données ici
-        setProducts(data);
-      })
+      .then((data) => setProducts(data))
       .catch((error) =>
         console.error("Erreur lors du chargement des produits :", error)
       );
   }, []);
-  
 
   return (
     <div className="shop">
-      <Navbar />
+      <nav className="navbar">
+        <div className="brand">
+          <Link to="/">CHRONOLITHE.</Link>
+        </div>
+        <ul>
+          <li>
+            <Link to="/shop">Shop</Link>
+          </li>
+          <li>
+            <Link to="/about">À Propos</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
       <h1 className="shop-title">Notre Collection</h1>
       <div className="products-grid">
         {products.map((product) => (
           <div key={product.id} className="product-card">
             <img
-              src={product.images[0]}
+              src={product.images[0]} // Utilise seulement la première image
               alt={product.name}
               className="product-image"
             />
             <h2 className="product-name">{product.name}</h2>
             <p className="product-price">{product.price.toFixed(2)} €</p>
             <div className="product-colors">
-              {product.colors.map((color, index) => (
-                <span
-                  key={index}
-                  className="color-circle"
-                  style={{ backgroundColor: color }}
-                ></span>
-              ))}
-            </div>
+  {product.colors.map((color, index) => (
+    <div key={index} className="color-square" style={{ backgroundColor: color }}>
+      <div></div>
+    </div>
+  ))}
+</div>
+
           </div>
         ))}
       </div>
